4203ce51fceeb643323cf957c221e3623be38387
DimonUN <92425351+DimonUN@users.noreply.github.com>
2021-12-09 11:25:45 +0300

    Из проекта удален файл Main.storyboard. Добавлен UITabBarController,в него добавлено два UINavigationController'а.
    У добавленных контроллеров изменены Tab bar Item'ы.
    Созданы FeedViewController и ProfileViewController и добавлены в качестве root view controller'ов в навигационные контроллеры.
    Добавлен PostViewController для экрана с постом , у него изменен заголовок и цвет главной view.
    На FeedViewController добавлена кнопка, по которой осуществлен переход на экран поста.
    В FeedViewController создана структура, которая передает свое значение вPostViewController.
    На PostViewController добавлена Bar Button Item, при нажатии на неё, модально показывается InfoViewController.
    
    На InfoViewController добавлена кнопка, которая вызывает UIAlertController с двумя UIAlertAction, при нажатии на которые в консоль выводятся сообщения.

---
+++ Navigation/FeedViewController.swift
@@ -0,0 +1,42 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by Дмитрий Никоноров on 01.12.2021.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    struct Post {
+        let title: String
+    }
+    
+    var post = Post(title: "Первый пост")
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        addButton()
+    }
+
+    fileprivate func addButton() {
+        let button = UIButton()
+        button.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        button.center = self.view.center
+        button.backgroundColor = UIColor.systemBlue
+        button.setTitle("Пост", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        self.view.addSubview(button)
+    }
+    
+    @objc func buttonAction(sender: UIButton) {
+        let vc = PostViewController()
+        vc.firstPost = self.post.title
+        self.navigationController?.pushViewController(vc, animated: true)
+     }
+        
+}
---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Дмитрий Никоноров on 01.12.2021.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+        
+    }
+}
